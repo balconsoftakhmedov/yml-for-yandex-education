99e47e5583a1532ef5744a1381d5a94f5db422ae
Rusty <tutyou1972@gmail.com>
2024-05-03 23:33:44 +0500

    Update yml-for-yandex-education.php

--- yml-for-yandex-education.php
+++ yml-for-yandex-education.php
@@ -21,10 +21,25 @@ define( 'YML_FOR_YANDEX_EDUCATION_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 require_once YML_FOR_YANDEX_EDUCATION_PLUGIN_PATH . 'includes/class-yml-for-yandex-generate.php';
 require_once YML_FOR_YANDEX_EDUCATION_PLUGIN_PATH . 'includes/class-yml-for-yandex-education.php';
 
+// Schedule the cron job on plugin activation
+register_activation_hook( __FILE__, 'yml_for_yandex_education_schedule_event' );
+function yml_for_yandex_education_schedule_event() {
 
-// Инициализируем плагин
-function yml_for_yandex_education_init() {
-    $plugin = new YML_For_Yandex_Education();
-    $plugin->run();
+	if ( ! wp_next_scheduled( 'yml_for_yandex_education_daily_event' ) ) {
+
+		wp_schedule_event( time(), 'daily', 'yml_for_yandex_education_daily_event' );
+	}
 }
-add_action( 'init', 'yml_for_yandex_education_init' );
+
+add_action( 'init', 'yml_for_yandex_education_schedule_event' );
+add_action( 'yml_for_yandex_education_daily_event', 'yml_for_yandex_education_init' );
+function yml_for_yandex_education_init() {
+	$plugin = new YML_For_Yandex_Education();
+	$plugin->run();
+}
+
+register_deactivation_hook( __FILE__, 'yml_for_yandex_education_unschedule_event' );
+function yml_for_yandex_education_unschedule_event() {
+
+	wp_clear_scheduled_hook( 'yml_for_yandex_education_daily_event' );
+}
